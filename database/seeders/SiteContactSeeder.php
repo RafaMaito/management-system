52243da8e3a472f3200d63df7b9dd10bbe4a23d3
Rafael Maito <rafaelmaito7@gmail.com>
2025-03-22 01:35:32 -0300

    MM-05: Site Contact Factory to create 10 registers in SCSeeder

--- database/seeders/SiteContactSeeder.php
+++ database/seeders/SiteContactSeeder.php
@@ -14,20 +14,25 @@ class SiteContactSeeder extends Seeder
      */
     public function run(): void
     {
-        SiteContact::create([
-            'name' => 'Test Contact',
-            'phone' => '1234567890',
-            'email' => 'testcontact@testemail.com',
-            'contact_reason' => 1,
-            'message' => 'Test Message',
-        ]);
+        // SiteContact::create([
+        //     'name' => 'Test Contact',
+        //     'phone' => '1234567890',
+        //     'email' => 'testcontact@testemail.com',
+        //     'contact_reason' => 1,
+        //     'message' => 'Test Message',
+        // ]);
 
-        $contact = new SiteContact();
-        $contact->name = 'Test Contact 2';
-        $contact->phone = '0987654321';
-        $contact->email = 'testcontact2@testemail.com.br';
-        $contact->contact_reason = 2;
-        $contact->message = 'Test Message 2';
-        $contact->save();
+        // $contact = new SiteContact();
+        // $contact->name = 'Test Contact 2';
+        // $contact->phone = '0987654321';
+        // $contact->email = 'testcontact2@testemail.com.br';
+        // $contact->contact_reason = 2;
+        // $contact->message = 'Test Message 2';
+        // $contact->save();
+
+        // Create 10 new site contact records using the factory.
+        // The factory will use the SiteContactFactory class to generate
+        // the data for the records.
+        SiteContact::factory(10)->create();
     }
 }
