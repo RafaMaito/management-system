e0c98a2ec49cd6e22af14fcd5961e8755ba6c7f5
Rafael Maito <rafaelmaito7@gmail.com>
2025-03-22 01:17:26 -0300

    MM-05: Seeders SC fixed

--- database/seeders/SiteContactSeeder.php
+++ database/seeders/SiteContactSeeder.php
@@ -18,7 +18,7 @@ class SiteContactSeeder extends Seeder
             'name' => 'Test Contact',
             'phone' => '1234567890',
             'email' => 'testcontact@testemail.com',
-            'contact_reason' => 'Test Reason',
+            'contact_reason' => 1,
             'message' => 'Test Message',
         ]);
 
@@ -26,7 +26,7 @@ class SiteContactSeeder extends Seeder
         $contact->name = 'Test Contact 2';
         $contact->phone = '0987654321';
         $contact->email = 'testcontact2@testemail.com.br';
-        $contact->contact_reason = 'Test Reason 2';
+        $contact->contact_reason = 2;
         $contact->message = 'Test Message 2';
         $contact->save();
     }
