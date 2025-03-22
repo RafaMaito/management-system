81b6af6c20e1d505f31a4b8351f79baff83a3990
Rafael Maito <rafaelmaito7@gmail.com>
2025-03-22 01:04:02 -0300

    MM-05: Supplier seeder created to create two registers in the database

---
+++ database/seeders/SupplierSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Supplier;
+
+class SupplierSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void {
+        // Instructs to register the model events in the database seeder.
+        $this->withoutEvents(function () {
+            // Create a new supplier record.
+            Supplier::create([
+                'name' => 'Test Supplier',
+                'site' => 'Test Supplier',
+                'uf' => 'MG',
+                'email' => 'test@testemail.com',
+            ]);
+        });
+
+        // Insert a new supplier record using the query builder.
+        DB::table('suppliers')->insert([
+            'name' => 'Test Supplier 2',
+            'site' => 'Test Supplier 2',
+            'uf' => 'RS',
+            'email' => 'test2@emailtest.com.br',
+        ]);
+    }
+}
