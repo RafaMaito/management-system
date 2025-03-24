49f6a6f2e2784568a7ce035280de68b0d4d7ed1c
Rafael Maito <rafaelmaito7@gmail.com>
2025-03-23 22:56:48 -0300

    MM-06: Contact form errors improved

--- resources/views/site/layouts/_components/form_contact.blade.php
+++ resources/views/site/layouts/_components/form_contact.blade.php
@@ -2,10 +2,13 @@
 <form action={{ route('site.contact') }} method="post">
     @csrf
     <input name="name" value="{{ old('name')}}" type="text" placeholder="Name" class="{{ $border }}">
+    {{ $errors->has('name') ? $errors->first('name') : ''}}
     <br>
     <input name="phone" value="{{ old('phone')}}" type="text" placeholder="Phone" class="{{ $border }}">
+    {{ $errors->has('phone') ? $errors->first('phone') : ''}}
     <br>
     <input name="email" value="{{ old('email')}}" type="text" placeholder="Email" class="{{ $border }}">
+    {{ $errors->has('email') ? $errors->first('email') : ''}}
     <br>
     <select name="contact_reasons_id" class="{{ $border }}">
         <option value="">What is the reason for your contact?</option>
@@ -13,8 +16,10 @@
             <option value="{{ $contact_reason->id }}" {{ old('contact_reasons_id') == $contact_reason->id ? 'selected' : ''}} >{{ $contact_reason->contact_reason }}</option>
         @endforeach
     </select>
+    {{ $errors->has('contact_reasons_id') ? $errors->first('contact_reasons_id') : ''}}
     <br>
     <textarea name="message" class="{{ $border }}">{{ old('message') ?: 'Enter your message here' }}</textarea>
+    {{ $errors->has('message') ? $errors->first('message') : ''}}
     <br>
     <button type="submit" class="{{ $border }}">SEND</button>
 </form>
