decb82173f45f9e749dec06e34101eb08e0a9f13
Rafael Maito <rafaelmaito7@gmail.com>
2025-03-24 14:39:53 -0300

    MM-08: Users views created

---
+++ resources/views/market/clienthome.blade.php
@@ -0,0 +1,12 @@
+@extends('market.layouts.base')
+
+@section('title', $title)
+
+@section('content')
+
+<div class="page-content">
+    <div class="page-title">
+        <h1>Get in Touch with Us</h1>
+    </div>
+</div>
+@endsection
---
+++ resources/views/market/layouts/base.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>Management System - @yield('title')</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    </head>
+
+    <body>
+        @include('market.layouts._partials.header')
+        @yield('content')
+        {{-- @unless (Route::is('site.home'))
+            @include('site.layouts._partials.footer')
+        @endunless --}}
+    </body>
+</html>
