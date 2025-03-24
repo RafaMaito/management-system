4b1ca2c6c5b5e9161b1bb903d4b76e3633054a77
Rafael Maito <rafaelmaito7@gmail.com>
2025-03-24 14:41:52 -0300

    MM-08: Reorganized routes

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,17 @@ Route::get('/login/{error?}', [\App\Http\Controllers\LoginController::class,'ind
 Route::post('/login', [\App\Http\Controllers\LoginController::class,'authenticate'])->name('site.login');
 
 Route::middleware('market')->prefix('market')->group(function() {
-    Route::get('/users', function () { return 'Users'; })->name('market.users');
+    Route::get('/user', function () { return 'Users'; })->name('market.user');
 
-    Route::get('/clients', function () { return 'Clients'; })->name('market.clients');
+    Route::get('/client-home', [\App\Http\Controllers\ClientHomeController::class,'index'])->name('market.clienthome');
 
-    Route::get('/suppliers',[\App\Http\Controllers\SupplierController::class,'index'])->name('market.supplier');
+    Route::get('/logout', [\App\Http\Controllers\LoginController::class,'logout'])->name('market.logout');
 
-    Route::get('/products', function () { return 'Products'; })->name('market.products');
+    Route::get('/client', [\App\Http\Controllers\ClientController::class,'index'])->name('market.client');
+
+    Route::get('/supplier',[\App\Http\Controllers\SupplierController::class,'index'])->name('market.supplier');
+
+    Route::get('/product', [\App\Http\Controllers\ProductController::class,'index'])->name('market.product');
 });
 
 Route::fallback(function() {
