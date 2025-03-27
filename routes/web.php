612e2e1fc5a4f8a9b0f4768a9ddcffbb0160923b
Rafael Maito <rafaelmaito7@gmail.com>
2025-03-27 02:19:16 -0300

    MM-09: Edit route created

--- routes/web.php
+++ routes/web.php
@@ -3,32 +3,33 @@
 use Illuminate\Support\Facades\Route;
 
 // using an string in the callback() place the laravel wait a controler method.
-Route::get('/', [\App\Http\Controllers\HomeController::class, 'home'])->name('site.home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'home'])->name('site.home');
 
-Route::get('/about-us', [\App\Http\Controllers\AboutUsController::class, 'aboutus'])->name('site.aboutus');
+Route::get('/about-us', [App\Http\Controllers\AboutUsController::class, 'aboutus'])->name('site.aboutus');
 
-Route::get('/contact', [\App\Http\Controllers\ContactController::class, 'contact'])->name('site.contact');
-Route::post('/contact', [\App\Http\Controllers\ContactController::class, 'save'])->name('site.contact');
+Route::get('/contact', [App\Http\Controllers\ContactController::class, 'contact'])->name('site.contact');
+Route::post('/contact', [App\Http\Controllers\ContactController::class, 'save'])->name('site.contact');
 
-Route::get('/login/{error?}', [\App\Http\Controllers\LoginController::class, 'index'])->name('site.login');
-Route::post('/login', [\App\Http\Controllers\LoginController::class, 'authenticate'])->name('site.login');
+Route::get('/login/{error?}', [App\Http\Controllers\LoginController::class, 'index'])->name('site.login');
+Route::post('/login', [App\Http\Controllers\LoginController::class, 'authenticate'])->name('site.login');
 
 Route::middleware('market')->prefix('market')->group(function () {
     Route::get('/user', function () {
         return 'Users';
     })->name('market.user');
 
-    Route::get('/client-home', [\App\Http\Controllers\ClientHomeController::class, 'index'])->name('market.clienthome');
+    Route::get('/client-home', [App\Http\Controllers\ClientHomeController::class, 'index'])->name('market.clienthome');
 
-    Route::get('/logout', [\App\Http\Controllers\LoginController::class, 'logout'])->name('market.logout');
+    Route::get('/logout', [App\Http\Controllers\LoginController::class, 'logout'])->name('market.logout');
 
-    Route::get('/client', [\App\Http\Controllers\ClientController::class, 'index'])->name('market.client');
+    Route::get('/client', [App\Http\Controllers\ClientController::class, 'index'])->name('market.client');
 
-    Route::get('/supplier', [\App\Http\Controllers\SupplierController::class, 'index'])->name('market.supplier');
-    Route::match(['get', 'post'], '/supplier/register', [\App\Http\Controllers\SupplierController::class, 'register'])->name('market.supplier.register');
-    Route::match(['get', 'post'], '/supplier/list', [\App\Http\Controllers\SupplierController::class, 'list'])->name('market.supplier.list');
+    Route::get('/supplier', [App\Http\Controllers\SupplierController::class, 'index'])->name('market.supplier');
+    Route::match(['get', 'post'], '/supplier/register', [App\Http\Controllers\SupplierController::class, 'register'])->name('market.supplier.register');
+    Route::match(['get', 'post'], '/supplier/list', [App\Http\Controllers\SupplierController::class, 'list'])->name('market.supplier.list');
+    Route::match(['get', 'post'], '/supplier/edit/{id}/{msg?}', [App\Http\Controllers\SupplierController::class, 'edit'])->name('market.supplier.edit');
 
-    Route::get('/product', [\App\Http\Controllers\ProductController::class, 'index'])->name('market.product');
+    Route::get('/product', [App\Http\Controllers\ProductController::class, 'index'])->name('market.product');
 });
 
 Route::fallback(function () {
