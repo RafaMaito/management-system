6055b8ebe22ff3f6892f4631c1ccf4a2a7a90059
Rafael Maito <rafaelmaito7@gmail.com>
2025-03-28 05:00:11 -0300

    MM-09: Resourse functions added to Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,78 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\Unit;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
     {
-        return view('market.product.index', ['title' => 'Product']);
+        $products = Product::paginate(5);
+
+        return view('market.product.index', ['title' => 'Products', 'products' => $products, 'request' => $request->all()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Request $request)
+    {
+        $units = Unit::all();
+        $button_func = 'Create';
+
+        return view('market.product.create', ['title' => 'Create a Product', 'button_func' => $button_func, 'units' => $units]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|min:3|max:40',
+            'description' => 'required|min:3|max:2000',
+            'weight' => 'required|integer',
+            'unit_id' => 'exists:units,id',
+        ]);
+
+        Product::create($request->all());
+
+        return redirect()->route('product.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+
+        return view('market.product.show', ['title' => 'Product', 'product' => $product]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
     }
 }
