719d21b728972744ed0fb5b6ec5b29bf850b44cc
Rafael Maito <rafaelmaito7@gmail.com>
2025-04-13 19:35:41 -0300

    MM-10: Order model/controller created

---
+++ app/Models/Order.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    //
+
+    public function clents()
+    {
+        return $this->hasMany(Order::class);
+    }
+}
