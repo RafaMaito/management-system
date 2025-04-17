04f03a47158651a4e27ecc6cb91f7723e6dfb69d
Rafael Maito <rafaelmaito7@gmail.com>
2025-04-16 23:20:02 -0300

    MM-11: Share errors middleware added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,36 +11,22 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-
         // Middleware aliases.
         $middleware->alias([
-
         ]);
 
         // Middleware global of the application (executed in all routes).
         $middleware->group('web', [
-            \App\Http\Middleware\LogAccessMiddleware::class,
-            // Initiate the session for thhe old session
-            \Illuminate\Session\Middleware\StartSession::class,
-
             // Share the errors from the session with the views
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-
-            // CSRF Protection
-            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
-
+            Illuminate\View\Middleware\ShareErrorsFromSession::class,
         ]);
 
         // Middleware group "api" of the application (executed in all routes).
         $middleware->group('api', [
-
         ]);
 
         $middleware->group('market', [
-            \App\Http\Middleware\AuthenticationMiddleware::class,
-         ]);
-
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
     })->create();
